1da19cbfe023b286515130ff5f89f25761aef89f
Valinthieth <ishfaqmcb12@gmail.com>
2024-10-24 14:04:14 -0400

    Commiting Changes

--- src/assets/works.js
+++ src/assets/works.js
@@ -3,23 +3,23 @@
 export const filesArray = [
     
     {
-    title: "Portrait of a Writer",
-    description: "An essay highlighting my writing practices, my writing journey, and my writing development",
-    link: "sample",
+    title: "Portrait of a Writer, First Draft",
+    description: "An essay highlighting my writing practices, my writing journey, and my writing development, first draft.",
+    link: "https://docs.google.com/document/d/1d6XwG6J1OycrkNHET93I7a4w4JJon5IhzsxuQcv2Dnk/edit?usp=sharing",
     id:'1'
     },
 
     {
-        title: "My Discourse Community",
-        description: "An essay discussing what a Discourse Community is and my own Discourse Community that I am a part of",
-        link: "sample",
+        title: "Portrait of a Writer, Final Draft",
+        description: "An essay highlighting my writing practices, my writing journey, and my writing development, final draft.",
+        link: "https://docs.google.com/document/d/1IUGIBj1CtFDUlyf_iXVnmS5JHd_0FsW8y8mL2CK6IIY/edit?usp=sharing",
         id:'2'
     },
 
     {
-        title: "Major Project 3",
-        description: "Sample",
-        link: "sample",
+        title: "Reflection",
+        description: "A reflection piece where I discuss my writing choices I took when writing the piece, 'Portrait of a Writer'. ",
+        link: "https://docs.google.com/document/d/1gcqhYqsin_PZLItCKykBaShmi_GQULgJWb_kBc7GbEo/edit?usp=sharing",
         id:'3'
     },
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,20 +6,24 @@ export const Work = () => {
     <div className='flex text-center justify-center items-center mt-32'>
     {
         filesArray.map((work) => (
-            <div key={work.id} className='pr-4'>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white h-52 w-80">
+            <div key={work.id} className='pr-4 text-white'>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-600 h-56 w-80">
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{work.title}</div>
         
-              <p className="text-gray-700 text-base">{work.description}</p> 
+              <p className=" text-base">{work.description}</p> 
+
+              <a href={work.link} target='_blank'>
+              <button className="bg-blue-500 text-white font-bold py-2 px-4 mt-5 rounded hover:bg-blue-700">
+                View 
+              </button>
+              </a>
+            
             
             </div>
         
-            <div className="h-10">
-              <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
-                Learn More
-              </button>
-            </div>
+            
+           
           </div>
           </div>
         ))
